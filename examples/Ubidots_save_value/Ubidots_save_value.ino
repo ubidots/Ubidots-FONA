18e0f1f6b036c9cba06b4a0ab5f4a990844f998f
metavix <metavixmv@gmail.com>
2016-01-21 16:02:22 -0500

    now the examples has define ID and define TOKEN

--- examples/Ubidots_save_value/Ubidots_save_value.ino
+++ examples/Ubidots_save_value/Ubidots_save_value.ino
@@ -2,8 +2,10 @@
 #define APN "Your_apn_of_your_SIM_here" 
 #define USER ""  // If your apn doesnt have username just put ""
 #define PASS ""  // If your apn doesnt have password just put ""
+#define TOKEN "CCN8GrVulRYGulPTk234R9Myx8qN2o"  // Remplace it with your token
+#define ID "" // Remplace it with your variable ID
 
-Ubidots client("CCN8GrVulRYGulPTk234R9Myx8qN2o");  // Remplace it with your token
+Ubidots client(TOKEN);  
   
 void setup() {
  //For FONA MODULE
@@ -17,6 +19,6 @@ void loop() {
   client.flushSerial();
   client.checkFona();
   client.gprsOnFona();
-  client.save_value(value,"569801a77aa5423a8d09dc3f");  // Remplace it with your variable ID
+  client.save_value(value, ID);  
   delay(600);  // 600 milliseconds 
 }
