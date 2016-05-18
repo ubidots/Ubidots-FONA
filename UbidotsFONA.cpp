fa9debd1604ae42ff21abd3418bf5f0dacd82ebd
Metavix <Metavix@localhost.localdomain>
2016-05-18 16:02:24 -0500

    changed headers of the library for arduino UNO compile

--- UbidotsFONA.cpp
+++ UbidotsFONA.cpp
@@ -1,10 +1,19 @@
-#include "UbidotsFONA.h"
+
+#include <Stream.h>
+#include <avr/pgmspace.h>
+#ifdef ARDUINO_ARCH_AVR
+#include <avr/wdt.h>
+#else
+#define wdt_reset()
+#endif
 #include <stdlib.h>
+
+#include "UbidotsFONA.h"
 /**
  * Constructor.
  */
  
-Ubidots::Ubidots(char* token, char* server = NULL){
+Ubidots::Ubidots(char* token, char* server){
     _token = token;
     _server = server;
     _dsName = NULL;
@@ -13,7 +22,7 @@ Ubidots::Ubidots(char* token, char* server = NULL){
     val = (Value *)malloc(MAX_VALUES*sizeof(Value));
     fonaSS.begin(4800);
 }
-void Ubidots::begin() {
+/*void Ubidots::begin() {
     pinMode(FONA_RST, OUTPUT);
     digitalWrite(FONA_RST, HIGH);
     delay(10);
@@ -21,7 +30,7 @@ void Ubidots::begin() {
     delay(100);
     digitalWrite(FONA_RST, HIGH);
     delay(500);
-}
+}*/
 
 void Ubidots::setDataSourceName(char* dsName) {
     _dsName = dsName;
@@ -33,11 +42,11 @@ void Ubidots::setDataSourceTag(char* dsTag) {
 
 void Ubidots::add(char *variable_id, float value, char *ctext1) {
     (val+currentValue)->idName = variable_id;
-    (val+currentValue)->ctext = contextOne;
-    (val+currentValue)->varValue = idValue;
+    (val+currentValue)->contextOne = ctext1;
+    (val+currentValue)->idValue = value;
     currentValue++;
-    if (currentValue > MAX_VALUE) {
-        currentValue = MAX_VALUE;
+    if (currentValue > MAX_VALUES) {
+        currentValue = MAX_VALUES;
     }
 }
 
@@ -62,58 +71,58 @@ bool Ubidots::waitForOK(uint16_t timeout) {
 bool Ubidots::setApn(char* apn, char* user, char* pwd) {
     fonaSS.println("AT");
     if (!waitForOK(6000)) {
-        SerialUSB.println("------>AT");
+        Serial.println("------>AT");
         return false;
     }
     fonaSS.println("AT+CSQ");
     if (!waitForOK(6000)) {
-        SerialUSB.println("Error at CSQ");
+        Serial.println("Error at CSQ");
         return false;
     }
     fonaSS.println("AT+CIPSHUT");
     if (!waitForOK(6000)) {
-        SerialUSB.println("Error at CIPSHUT");
+        Serial.println("Error at CIPSHUT");
         return false;
     }
     fonaSS.println("AT+CGATT?");
     if (!waitForOK(6000)) {
-        SerialUSB.println("GPRS is not attached");
+        Serial.println("GPRS is not attached");
         return false;
     }
     fonaSS.println("AT+SAPBR=3,1,\"CONTYPE\",\"GPRS\"");
     if (!waitForOK(6000)) {
-        SerialUSB.println("Error at setting up CONTYPE");
+        Serial.println("Error at setting up CONTYPE");
         return false;
     }
     fonaSS.print("AT+SAPBR=3,1,\"APN\",\"");
     fonaSS.print(apn);
     fonaSS.println("\"");
     if (!waitForOK(6000)) {
-        SerialUSB.println("Error at setting up APN");
+        Serial.println("Error at setting up APN");
         return false;
     }
     fonaSS.print("AT+SAPBR=3,1,\"USER\",\"");
     fonaSS.print(user);
     fonaSS.println("\"");
     if (!waitForOK(6000)) {
-        SerialUSB.println("Error at setting up apn user");
+        Serial.println("Error at setting up apn user");
         return false;
     }
     fonaSS.print("AT+SAPBR=3,1,\"PWD\",\"");
     fonaSS.print(pwd);
     fonaSS.println("\"");
     if (!waitForOK(6000)) {
-        SerialUSB.println("Error at setting up apn pass");
+        Serial.println("Error at setting up apn pass");
         return false;
     }
     fonaSS.println("AT+SAPBR=1,1");
     if (!waitForOK(6000)) {
-        SerialUSB.println("Error with AT+SAPBR=1,1 Connection ip");
+        Serial.println("Error with AT+SAPBR=1,1 Connection ip");
         return false;
     }
     fonaSS.println("AT+SAPBR=2,1");
     if (!waitForOK(6000)) {
-        SerialUSB.println("Error with AT+SAPBR=2,1 no IP to show");
+        Serial.println("Error with AT+SAPBR=2,1 no IP to show");
         return false;
     }
     return true;
@@ -135,13 +144,13 @@ bool Ubidots::sendAll() {
     }
     all += "=>";
     for (i = 0; i < currentValue; ) {
-        str = String(((val + i)->varValue), 2);
-        all += String((val + i)->varName);
+        str = String(((val + i)->idValue), 2);
+        all += String((val + i)->idName);
         all += ":";
         all += str;
-        if ((val + i)->ctext != NULL) {
+        if ((val + i)->contextOne != NULL) {
             all += "$";
-            all += String((val + i)->ctext);
+            all += String((val + i)->contextOne);
         }
         all += ","; 
         i++;
@@ -150,7 +159,7 @@ bool Ubidots::sendAll() {
     Serial.println(all.c_str());
     fonaSS.println("AT+CIPMUX=0");
     if (!waitForOK(6000)) {
-        SerialUSB.println("Error CIPMUX=0");
+        Serial.println("Error CIPMUX=0");
         return false;
     }
     fonaSS.print("AT+CIPSTART=\"TCP\",\"");
@@ -163,26 +172,52 @@ bool Ubidots::sendAll() {
     fonaSS.print(PORT);
     fonaSS.println("\"");
     if (!waitForOK(6000)) {
-        SerialUSB.println("Error at CIPSTART");
+        Serial.println("Error at CIPSTART");
         return false;
     }
     fonaSS.println("AT+CIPSEND");
     if (!waitForMessage(">", 6000)) {
-        SerialUSB.println("Error at CIPSEND");
+        Serial.println("Error at CIPSEND");
         return false;
     }
     fonaSS.write(all.c_str());
     fonaSS.write(0x1A);
     if (!waitForMessage("SEND OK", 6000)) {
-        SerialUSB.println("Error sending the message");
+        Serial.println("Error sending the message");
         return false;
     }
     fonaSS.println("AT+CIPCLOSE");
     if (!waitForMessage("CLOSE OK", 6000)) {
-        SerialUSB.println("Error closing TCP connection");
+        Serial.println("Error closing TCP connection");
         return false;
     }
     return true;
+}
+
+float Ubidots::getValueWithDatasource(char* dsTag, char* idName) {
+    float num;
+    int i = 0;
+    char allData[300];
+    String response;
+    uint8_t bodyPosinit;
+    sprintf(allData, "%s%s|LV|%s|%s:%s|end", USER_AGENT, VERSION, _token, dsTag, idName);
+    fonaSS.println("AT+CIPSEND");
+    if (!waitForMessage(">", 6000)) {
+        Serial.println("Error at CIPSEND");
+        return false;
+    }
+    fonaSS.write(allData);
+    fonaSS.write(0x1A);
+    if (!waitForMessage("SEND OK", 6000)) {
+        Serial.println("Error sending the message");
+        return false;
+    }
+    bodyPosinit = 3 + response.indexOf("OK|");
+    response = response.substring(bodyPosinit);
+    num = response.toFloat();
+    currentValue = 0;
+    free(allData);
+    return num;
 }
 
 bool Ubidots::waitForMessage(const char *msg, uint32_t ts_max) {
@@ -197,6 +232,47 @@ bool Ubidots::waitForMessage(const char *msg, uint32_t ts_max) {
         }
     }
     return false;         // This indicates: timed out
+}
+
+int Ubidots::readLine(uint32_t ts_max) {
+    uint32_t ts_waitLF = 0;
+    bool seenCR = false;
+    int c;
+    size_t bufcnt;
+    bufcnt = 0;
+    while (!isTimedOut(ts_max)) {
+        wdt_reset();
+        if (seenCR) {
+            c = fonaSS.peek();
+            // ts_waitLF is guaranteed to be non-zero
+            if ((c == -1 && isTimedOut(ts_waitLF)) || (c != -1 && c != '\n')) {
+                // Line ended with just <CR>. That's OK too.
+                goto ok;
+            }
+      // Only \n should fall through
+        }
+        c = fonaSS.read();
+        if (c < 0) {
+            continue;
+        }
+        Serial.print((char)c);                 // echo the char
+        seenCR = c == '\r';
+        if (c == '\r') {
+            ts_waitLF = millis() + 50;        // Wait another .05 sec for an optional LF
+        } else if (c == '\n') {
+            goto ok;
+        } else {
+            // Any other character is stored in the line buffer
+            if (bufcnt < (DEFAULT_BUFFER_SIZE - 1)) {    // Leave room for the terminating NUL
+                buffer[bufcnt++] = c;
+            }
+        }
+    }
+    Serial.println(F("readLine timed out"));
+    return -1;            // This indicates: timed out
+    ok:
+    buffer[bufcnt] = 0;     // Terminate with NUL byte
+    return bufcnt;
 }
 
 void Ubidots::turnOnFona(){
--- UbidotsFONA.h
+++ UbidotsFONA.h
@@ -3,7 +3,10 @@
 #define DEBUG_UBIDOTS
 
 #include <SoftwareSerial.h>
+#include <stdlib.h>
+#include "Arduino.h"
 
+#define DEFAULT_BUFFER_SIZE      64
 #define FONA_RX 2
 #define FONA_TX 3
 #define FONA_RST 4
@@ -13,6 +16,7 @@
 #define PORT "9010"
 #define USER_AGENT "FONA"
 #define VERSION "/1.1"
+#define MAX_VALUES 5
 
 
 typedef struct Value {
@@ -24,13 +28,17 @@ typedef struct Value {
 
 class Ubidots{
 private:
+    SoftwareSerial fonaSS = SoftwareSerial(FONA_TX, FONA_RX);
     bool waitForOK(uint16_t timeout=4000);
     bool waitForMessage(const char *msg, uint32_t ts_max);
-    SoftwareSerial fonaSS = SoftwareSerial(FONA_TX, FONA_RX);
+    bool isTimedOut(uint32_t ts) { return (long)(millis() - ts) >= 0; }
+    int readLine(uint32_t ts_max); 
     char* _token;
     char* _dsName;
-    char* _dsToken;
+    char* _dsTag;
     char* _server;
+    char buffer[DEFAULT_BUFFER_SIZE];
+    uint8_t currentValue;
     Value * val;
 
     
@@ -40,8 +48,9 @@ public:
     void begin();
     void setDataSourceName(char *dsName);
     void setDataSourceTag(char *dsTag);
-    void add(char *variable_id, double value, char *ctext1 = NULL);
-    void sendAll();
+    void add(char *variable_id, float value, char *ctext1 = NULL);
+    bool sendAll();
+    float getValueWithDatasource(char* dsTag, char* idName);
     bool setApn(char* apn, char* user = "", char* pwd = "");
 
     void turnOffFona();
