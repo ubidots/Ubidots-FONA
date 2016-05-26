9b17a47ee00d9cf81a3737e52cacf66193accddf
Metavix <Metavix@localhost.localdomain>
2016-05-26 15:44:01 -0500

    Deleted old examples and created new examples of get and save value with ID

--- examples/UbidotsGetValueWithID/UbidotsGetValueWithID.ino
+++ examples/UbidotsGetValueWithID/UbidotsGetValueWithID.ino
@@ -1,8 +1,5 @@
 #include <Ubidots_FONA.h>
-
-
-#include <SoftwareSerial.h> 
-#include <Adafruit_FONA.h>
+#include <SoftwareSerial.h>
 
 
 #define APN  ""  // The APN of your operator
@@ -11,19 +8,17 @@
 #define TOKEN "Your_token_here"  // Replace it with your Ubidots token
 #define ID "Your_id_here" // Replace it with your Ubidots' variable ID
 
+
 Ubidots client(TOKEN);  
   
 void setup() {
   Serial.begin(115200);
   delay(2000);
-  client.gprsNetwork(F(APN),F(USER),F(PASS));
+  client.setApn(APN, USER, PASS);
 }
 
 void loop() {
-  float value = analogRead(A0);  // Reading analog pin A0
-  client.flushSerial();
-  client.checkFona();
-  client.gprsOnFona();
-  client.saveValue(ID, value);  
+  float value = client.getValue(ID);  
   delay(1000); 
+  Serial.println(value);
 }
